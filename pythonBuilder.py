b2bc440c799d5ef26cbcd43fcb2844e48a1efcd8
Alec Rajeev <arajeev@rrl-imac-04.local>
2015-09-14 17:32:46 -0400

    added a thermalization step

--- pythonBuilder.py
+++ pythonBuilder.py
@@ -5,36 +5,35 @@ r_start = 2.0
 r_finish = 4.0
 r_step = .1
 
-n_steps = 1000
+n_thermal = 1000 # number of thermalization steps
+n_steps = 1000 # number of final steps
 
 def buildR():
 
-	data_array = np.array([])
 	r_array = np.array([])
 
 	for r in np.arange(r_start, r_finish, r_step):
-		r_array = np.append(r_array, r)
-		data_array = np.append(data_array, .5)
+		r_array = np.append(r_array, r) # builds array with every r value that will be used
 
-	n_size = np.size(r_array)
-	full_data_array = np.zeros([n_steps, n_size])
-	one_array = np.ones(n_size)
+	n_size = np.size(r_array) # the number of r's to be calculated (number of columns)
+	thermalization_data_array = np.zeros([n_thermal, n_size])
+	final_data_array = np.zeros([n_steps, n_size])
+	one_array = np.ones(n_size) # array with just ones
 
-	full_data_array[0] = one_array/2
+	thermalization_data_array[0] = one_array/2 # gives starting point for each as .5
+
+	for i in xrange(1, n_thermal):
+		thermalization_data_array[i] = (r_array*thermalization_data_array[i-1])*(one_array - thermalization_data_array[i-1])
+
+	final_data_array[0] = thermalization_data_array[n_thermal-1]
+
+	print final_data_array[0]
 
 	for i in xrange(1, n_steps):
-		full_data_array[i] = (r_array*full_data_array[i-1])*(one_array - full_data_array[i-1])
+		final_data_array[i] = (r_array*final_data_array[i-1])*(one_array - final_data_array[i-1])
 
-	np.savetxt("output.csv", full_data_array, delimiter=',', header=str(r_array.tolist())[1:-1], comments="")
+	np.savetxt("output.csv", final_data_array, delimiter=',', header=str(r_array.tolist())[1:-1], comments="")
 
-def calculateDataForSingleR(r):
-	start = .5
-	for i in xrange(0, 1000):
-		temp = r*start*(1-start)
-		start = temp
-		print temp
-
-	return start
 
 def main():
 	buildR()
